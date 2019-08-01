2108a3abbf34c83379bc561dcf35371586807ab5
Mujtaba Ahmed <mmuujji@gmail.com>
2019-08-01 11:57:24 +0500

    fake fetch applied

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
         // console.log(genreService);
         var vm = this;
-        vm.genreData = genreService.genreData;
+        vm.genreData = genreService.getGenreData();
 
         //Model
         vm.currentStep = 1;
@@ -37,7 +37,7 @@
         ];
         vm.currentGenre = {};
         // vm.newSubgenre = {};
-        vm.totalSubgenres = 24;
+        vm.totalSubgenres = ++genreService.getLastSubgenreId();
 
         vm.selectGenre = function(genre) {
             vm.currentGenre.selectedGenre = genre.id;
@@ -67,99 +67,155 @@
         }
         
         vm.save = function() {
-            // alert("Saving ...");
-            genreService.updateGenres(vm.currentGenre, vm.currentGenre.selectedGenre);
+            genreService.updateGenres(vm.currentGenre, vm.currentGenre.selectedGenre).then(function (msg) {
+                alert(msg);
+                vm.gotoStep(1);
+            }).catch(function(err) {
+                alert(err);
+                vm.gotoStep(1);
+            });
         }
     }
 
     function genreService() {
         this.genreData = {
             "genres": [
-            {
-            "id": 1,
-            "name": "Genre 1", "subgenres": [
-            {
-            "id": 10,
-            "name": "Subgenre 1",
-            "isDescriptionRequired": true },{
-            "id": 11,
-            "name": "Subgenre 2", "isDescriptionRequired": false
-            },{
-            "id": 12,
-            "name": "Subgenre 3",
-            "isDescriptionRequired": true },{
-            "id": 13,
-            "name": "Subgenre 4", "isDescriptionRequired": true
-            },{
-            "id": 14,
-            "name": "Subgenre 5","isDescriptionRequired": true }
-            ] },{
-            "id": 2,
-            "name": "Genre 2", "subgenres": [
-            {
-            "id": 15,
-            "name": "Subgenre 1",
-            "isDescriptionRequired": true },{
-            "id": 16,
-            "name": "Subgenre 2", "isDescriptionRequired": false
-            },{
-            "id": 17,
-            "name": "Subgenre 3",
-            "isDescriptionRequired": true }
-            ] },{
-            "id": 3,
-            "name": "Genre 3", "subgenres": [
-            {
-            "id": 18,
-            "name": "Subgenre 1",
-            "isDescriptionRequired": true },{
-            "id": 19,
-            "name": "Subgenre 2", "isDescriptionRequired": true
-            },{
-            "id": 20,
-            "name": "Subgenre 3","isDescriptionRequired": true }
-            ] },{
-            "id": 4,
-            "name": "Genre 4", "subgenres": [
-            {
-            "id": 21,
-            "name": "Subgenre 1",
-            "isDescriptionRequired": false },{
-            "id": 22,
-            "name": "Subgenre 2", "isDescriptionRequired": false
-            },{
-            "id": 23,
-            "name": "Subgenre 3",
-            "isDescriptionRequired": false }
-            ] },{
-            "id": 5,
-            "name": "Genre 5", "subgenres": [
-            {
-            "id": 24,
-            "name": "Subgenre 1",
-            "isDescriptionRequired": true }
-            ] }
-        ] };
+              {
+                "id": 1,
+                "name": "Genre 1",
+                "subgenres": [
+                  {
+                    "id": 10,
+                    "name": "Subgenre 1",
+                    "isDescriptionRequired": true
+                  },
+                  {
+                    "id": 11,
+                    "name": "Subgenre 2",
+                    "isDescriptionRequired": false
+                  },
+                  {
+                    "id": 12,
+                    "name": "Subgenre 3",
+                    "isDescriptionRequired": true
+                  },
+                  {
+                    "id": 13,
+                    "name": "Subgenre 4",
+                    "isDescriptionRequired": true
+                  },
+                  {
+                    "id": 14,
+                    "name": "Subgenre 5",
+                    "isDescriptionRequired": true
+                  }
+                ]
+              },
+              {
+                "id": 2,
+                "name": "Genre 2",
+                "subgenres": [
+                  {
+                    "id": 15,
+                    "name": "Subgenre 1",
+                    "isDescriptionRequired": true
+                  },
+                  {
+                    "id": 16,
+                    "name": "Subgenre 2",
+                    "isDescriptionRequired": false
+                  },
+                  {
+                    "id": 17,
+                    "name": "Subgenre 3",
+                    "isDescriptionRequired": true
+                  }
+                ]
+              },
+              {
+                "id": 3,
+                "name": "Genre 3",
+                "subgenres": [
+                  {
+                    "id": 18,
+                    "name": "Subgenre 1",
+                    "isDescriptionRequired": true
+                  },
+                  {
+                    "id": 19,
+                    "name": "Subgenre 2",
+                    "isDescriptionRequired": true
+                  },
+                  {
+                    "id": 20,
+                    "name": "Subgenre 3",
+                    "isDescriptionRequired": true
+                  }
+                ]
+              },
+              {
+                "id": 4,
+                "name": "Genre 4",
+                "subgenres": [
+                  {
+                    "id": 21,
+                    "name": "Subgenre 1",
+                    "isDescriptionRequired": false
+                  },
+                  {
+                    "id": 22,
+                    "name": "Subgenre 2",
+                    "isDescriptionRequired": false
+                  },
+                  {
+                    "id": 23,
+                    "name": "Subgenre 3",
+                    "isDescriptionRequired": false
+                  }
+                ]
+              },
+              {
+                "id": 5,
+                "name": "Genre 5",
+                "subgenres": [
+                  {
+                    "id": 24,
+                    "name": "Subgenre 1",
+                    "isDescriptionRequired": true
+                  }
+                ]
+              }
+            ]
+        };
 
         this.updateGenres = function (genre, id) {
-            // this fun is not even required in case of update, don't know why but this.genreData already contains update values like booktitle provided
             for (var i = 0; i < this.genreData.genres.length; i++) {
                 if (this.genreData.genres[i].id === id) {
-                    // this.genreData.genres[i].subgenres.push(genre.currentSubgenre);
                     for(var j = 0; j < this.genreData.genres[i].subgenres.length; j++) {
                         // update scenario
                         if (this.genreData.genres[i].subgenres[j].id === genre.currentSubgenre.id) {
                             this.genreData.genres[i].subgenres[j] = genre.currentSubgenre;
-                            break;
+                            return Promise.resolve("Book Updated Successfully!");
+                            // break;
                         }
                     }
                     if (j === this.genreData.genres[i].subgenres.length) {
                         // it means its add new book scenario as we have not found our id in already present subgenres
                         this.genreData.genres[i].subgenres.push(genre.currentSubgenre);
-                        break;
+                        return Promise.resolve("Book Added Successfully!");
+                        // break;
                     }
                 }
             }
+            return Promise.reject("Error");
+        }
+
+        this.getLastSubgenreId = function() {
+            return this.genreData.genres[this.genreData.genres.length - 1].subgenres[this.genreData.genres[this.genreData.genres.length - 1].subgenres.length-1].id;
+        }
+
+        this.getGenreData = function() {
+            return this.genreData;
         }
         
     }
